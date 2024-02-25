f367bffc09edaa125a741140cac83c6618eb5f8b
Anant-Chauhanx <anantchauhan0001@gmail.com>
2024-02-25 19:12:38 +0530

    Refactor subscribe form fetch URL to use REACT_APP_API_URL environment variable

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/subscribe', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/subscribe`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
