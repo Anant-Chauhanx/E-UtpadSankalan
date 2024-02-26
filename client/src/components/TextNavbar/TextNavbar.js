25f1abac085bc1ee13f7d70887bd0a7bb6dc5c10
Anant-Chauhanx <anantchauhan0001@gmail.com>
2024-02-26 14:44:22 +0530

    Add delay notice in TextNavbar due to free Render instance

--- client/src/components/TextNavbar/TextNavbar.js
+++ client/src/components/TextNavbar/TextNavbar.js
@@ -6,7 +6,7 @@ const FactBar = () => {
   return (
     <section className="TextNavbar">
       <div className="fact-content overflow-hidden">
-        <p>Did you know? E-waste is the worlds fastest-growing trash stream. Recycle your electronics responsibly!</p>
+        <p>If you're facing delay in requests, then it's likely due to the free Render instance I'm utilizing.</p>
       </div>
       <div className="learn-more">
         <Link className="learn-more-link" to="/faq">Learn More</Link>
